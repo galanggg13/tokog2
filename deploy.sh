6a14a545009070da1dc34cac72397708a76da606
galanggg13 <galangwahyuramadhani@gmail.com>
2025-12-02 20:58:41 +0700

    fix: enable deploy.sh + nixpacks

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
+
+echo "========================================"
+echo " RUNNING MIGRATIONS ON RAILWAY"
+echo "========================================"
+
 php artisan migrate --force
+
+echo "========================================"
+echo " MIGRATIONS DONE"
+echo "========================================"
+
+# start frankenphp
+php -S 0.0.0.0:8000 -t public
