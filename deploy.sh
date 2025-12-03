53a19d370d6c8a8d8ba92b554b0392d7935412f4
galanggg13 <galangwahyuramadhani@gmail.com>
2025-12-03 12:27:27 +0700

    add railway config

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
-echo "========================================"
-echo " RUNNING MIGRATIONS ON RAILWAY"
-echo "========================================"
+echo "================================"
+echo " RUNNING MIGRATIONS ON RAILWAY "
+echo "================================"
 
 php artisan migrate --force
 
-echo "========================================"
-echo " MIGRATIONS DONE"
-echo "========================================"
+echo "================================"
+echo " MIGRATIONS DONE "
+echo "================================"
 
-# start frankenphp
 php -S 0.0.0.0:8000 -t public
